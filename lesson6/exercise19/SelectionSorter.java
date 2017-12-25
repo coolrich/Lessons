eec0d3cd9b4bd01d042345472a7a82d85c926326
Ihor <i.domnich2013@gmail.com>
2017-12-25 10:27:43 +0200

    Modified

--- lesson6/exercise19/SelectionSorter.java
+++ lesson6/exercise19/SelectionSorter.java
@@ -5,25 +5,23 @@ import java.util.Arrays;
 public class SelectionSorter {
     public static void sort(int[] array) {
         boolean b = false;
-        for (int i = 0; i < array.length; i++) {    // i - номер текущего шага
 
+        for (int i = 0; i < array.length; i++) {    // i - номер текущего шага
             int pos = i;
             int min = array[i];
             // цикл выбора наименьшего элемента
             for (int j = i + 1; j < array.length; j++) {
-
                 if (array[j] < min) {
                     b = true;
                     pos = j;    // pos - индекс наименьшего элемента
                     min = array[j];
                 }
-
             }
-
             if (b) {
                 array[pos] = array[i];
                 array[i] = min;    // меняем местами наименьший с array[i]
                 System.out.println(Arrays.toString(array));
+                b = false;
             }
         }
     }
