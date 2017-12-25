2caad8ceecf0fa75ccdd47b55591fb632d511fe8
Ihor <i.domnich2013@gmail.com>
2017-12-25 10:39:37 +0200

    Modified

--- lesson2/CommentsAndLiterals.java
+++ lesson2/CommentsAndLiterals.java
@@ -1,16 +1,17 @@
 package lesson2;
+
 public class CommentsAndLiterals {
 
     public static void main(String[] args) {
-        boolean b=true;
-        String  s=  "s";
-        char    c=  'c';
-        int     i0= 0b1;
-        int     i1= 0b10;
-        int     i2= 0b11;
-        int     i3= 0b100;
-        float    f= 10.1f;
-        double   d= 10.2d;
+        boolean b = true;
+        String s = "s";
+        char c = 'c';
+        int i0 = 0b1;
+        int i1 = 0b10;
+        int i2 = 0b11;
+        int i3 = 0b100;
+        float f = 10.1f;
+        double d = 10.2d;
         System.out.println(s);
         System.out.println(c);
         System.out.println(i0);
@@ -20,5 +21,4 @@ public class CommentsAndLiterals {
         System.out.println(f);
         System.out.println(d);
     }
-
 }
