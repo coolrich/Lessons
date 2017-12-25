ed79ad1b1df6dd81b13a190652cbed7d11e5f962
Ihor <i.domnich2013@gmail.com>
2017-12-25 11:12:17 +0200

    Modified

---
+++ lesson2/Sum.java
@@ -0,0 +1,13 @@
+package lesson2;
+
+public class Sum {
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 0;
+        System.out.println(sum(a, b));
+    }
+}
