d6a22e036a5c24355e52e7e3bbb412fc31e58536
student <i.domnich2013@gmail.com>
2017-12-27 19:40:24 +0200

    Its my car

---
+++ lesson7/exercise1/classes/Car.java
@@ -0,0 +1,33 @@
+package lesson7.exercise1.classes;
+
+/**
+ * Created by student on 27.12.2017.
+ */
+public class Car {
+    private int weight;
+    private String color;
+
+    public Car() {
+        weight = 1000;
+        color = "Red";
+    }
+
+    public Car(int aWeight, String aColor) {
+        weight = aWeight;
+        color = aColor;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public static void main(String[] args) {
+        new Car().getWeight();
+        new Car().getColor();
+    }
+
+}
