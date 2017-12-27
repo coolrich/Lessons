34b80947c7f869ba86ff43527b2046eaa4e325f3
student <i.domnich2013@gmail.com>
2017-12-27 21:12:36 +0200

    Its my phone

--- lesson7/exercise1/home/Phone.java
+++ lesson7/exercise1/home/Phone.java
@@ -1,9 +1,13 @@
 package lesson7.exercise1.home;
 
 public class Phone {
-    public String number;
-    public String model;
-    public float weight;
+    private String number;
+    private String model;
+    private float weight;
+
+    public Phone() {
+
+    }
 
     public Phone(String number, String model, float weight) {
         this(number, model);
@@ -15,8 +19,12 @@ public class Phone {
         this.model = model;
     }
 
-    public Phone() {
+    public String getModel() {
+        return model;
+    }
 
+    public float getWeight() {
+        return weight;
     }
 
     public String getNumber() {
@@ -27,19 +35,18 @@ public class Phone {
         System.out.println(callerName + " is calling");
     }
 
-    public void receiveCall(String callerName,String callerNumber) {
-        System.out.println(callerName + " is calling");
+    public void receiveCall(String callerName, String callerNumber) {
+        System.out.println(callerName + " is calling\n" +callerNumber);
     }
+
+}
+
+class PhoneDemo {
 
     public static void main(String[] args) {
         Phone p1 = new Phone();
-        Phone p2 = new Phone();
-        Phone p3 = new Phone();
-        p1.weight = p2.weight = p3.weight = 100;
-        p1.model = p2.model = p3.model = "TT";
-        p1.number = "054644646";
-        p2.number = "050144874";
-        p3.number = "048785658";
+        Phone p2 = new Phone("0985462133", "AA");
+        Phone p3 = new Phone("0968524147", "A", 12);
 
         Phone[] all = new Phone[3];
         all[0] = p1;
@@ -48,9 +55,10 @@ public class Phone {
 
         for (Phone p :
                 all) {
-            System.out.println(p.model + " " + p.number + " " + p.weight);
+            System.out.println(p.getModel() + " " + p.getNumber() + " " + p.getWeight());
             p.getNumber();
-            p.receiveCall("Someone");
+            p.receiveCall("Someone","0964568987");
+            System.out.println("\n");
         }
     }
 }
