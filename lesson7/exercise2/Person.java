48a4ac4d04075a15be568d4b72999d778d4cef3c
Ihor <i.domnich2013@gmail.com>
2017-12-29 11:25:17 +0200

    Modified

--- lesson7/exercise2/Person.java
+++ lesson7/exercise2/Person.java
@@ -1,4 +1,26 @@
 package lesson7.exercise2;
 
 public class Person {
+    private String fullName;
+    int age;
+
+    public Person(){
+
+    }
+    public Person(String aFullName,int anAge){
+        fullName=aFullName;
+        age=anAge;
+    }
+
+    public void move(){
+        System.out.println(fullName+" is moving");
+    }
+    public void talk(){
+        System.out.println((fullName+" is talking"));
+    }
+
+    public static void main(String[] args) {
+        new Person();
+        new Person("Someone",30);
+    }
 }
