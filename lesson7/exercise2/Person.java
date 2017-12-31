e605177937109340682a6a797aabf3e65880da9f
Ihor <i.domnich2013@gmail.com>
2017-12-31 09:46:17 +0200

    Modified

--- lesson7/exercise2/Person.java
+++ lesson7/exercise2/Person.java
@@ -4,23 +4,25 @@ public class Person {
     private String fullName;
     int age;
 
-    public Person(){
+    public Person() {
 
     }
-    public Person(String aFullName,int anAge){
-        fullName=aFullName;
-        age=anAge;
+
+    public Person(String aFullName, int anAge) {
+        fullName = aFullName;
+        age = anAge;
     }
 
-    public void move(){
-        System.out.println(fullName+" is moving");
+    public void move() {
+        System.out.println(fullName + " is moving");
     }
-    public void talk(){
-        System.out.println((fullName+" is talking"));
+
+    public void talk() {
+        System.out.println((fullName + " is talking"));
     }
 
     public static void main(String[] args) {
-        new Person();
-        new Person("Someone",30);
+        new Person().move();
+        new Person("Someone", 30).talk();
     }
 }
