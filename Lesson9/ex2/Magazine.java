c11dd5e56d41fb61fc7360d6a1a8267ae3d034e3
Valerii Nochnyi <nochnyi.univer.kharkov@gmail.com>
2018-01-17 21:36:25 +0200

    none

--- Lesson9/ex2/Magazine.java
+++ Lesson9/ex2/Magazine.java
@@ -4,7 +4,7 @@ package Lesson9.ex2;
  * Created by Student on 17.01.2018.
  */
 public class Magazine implements Printable {
-
+//
     @Override
     public void print() {
         System.out.println("Magazine");
