41144d07a9b4582a553264e584eb9b8626a638a2
Ihor <i.domnich2013@gmail.com>
2018-02-09 23:01:34 +0200

    Revert "Modified"

--- lesson12/ex11and12and13/WordFinder.java
+++ lesson12/ex11and12and13/WordFinder.java
@@ -5,31 +5,31 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WordFinder {
-    public static List<StringBuilder> findSmallest(String str) {
+    public static List<String> findSmallest(String str) {
         Pattern pattern = Pattern.compile("[a-z]+", Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(str);
-        List<StringBuilder> strings = new ArrayList<StringBuilder>();
+        Map<Integer, Integer> wordsLength = new TreeMap<>();
+        List<String> words = new ArrayList<>();
         int count = 0;
         while (matcher.find()) {
-            strings.add(new StringBuilder());
+            Set<String> stringSet = new TreeSet<>();
             for (int i = 0; i < matcher.group().length(); i++) {
-                strings.get(count).append(matcher.group().charAt(i));
+                stringSet.add(String.valueOf(matcher.group().charAt(i)));
             }
-            count++;
+            words.add(matcher.group());
+            wordsLength.put(count++, stringSet.size());
         }
-        strings.sort(new Comparator<StringBuilder>() {
-            @Override
-            public int compare(StringBuilder o1, StringBuilder o2) {
-                return o1.length() > o2.length() ? 1 : -1;
+        int minNumOfLttrs = wordsLength.get(0);
+        for (int i = 1; i < wordsLength.size(); i++) {
+            minNumOfLttrs = minNumOfLttrs <= wordsLength.get(i) ? minNumOfLttrs : wordsLength.get(i);
+        }
+        List<String> smallestWords = new ArrayList<>();
+        for (int i = 0; i < words.size(); i++) {
+            if (wordsLength.get(i) <= minNumOfLttrs) {
+                smallestWords.add(words.get(i));
             }
-        });
-        count = 1;
-        List<StringBuilder> result = new ArrayList<StringBuilder>();
-        result.add(strings.get(0));
-        while (count < strings.size() && (strings.get(0).length() == strings.get(count).length())) {
-            result.add(strings.get(count++));
         }
-        return result;
+        return smallestWords;
     }
 
     public static int countOfLatinWords(String str) {
@@ -55,6 +55,6 @@ public class WordFinder {
     public static void main(String[] args) {
         System.out.println(countOfLatinWords("aaaaaaa abc  d 1 safdas sf f"));
         System.out.println(palindromeFinder("Если есть хвосты по дз, начните с 1 не сданного задания. 123 324 111 4554 1"));
-        System.out.println(findSmallest("afasdfs ffffffffffffffffff s aaaaa b"));
+        System.out.println(findSmallest("afasdfs ffffffffffffffffff s aaaaa b sdfghjk g g fghjk rtyu asertyuikl"));
     }
 }
