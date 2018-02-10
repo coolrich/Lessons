c66b04b386b1f6cc9e0fc604eb1d6951023a4293
Ihor <i.domnich2013@gmail.com>
2018-02-10 18:29:15 +0200

    Revert "Modified"

---
+++ lesson16/ex4/UniqeNums.java
@@ -0,0 +1,15 @@
+package lesson16.ex4;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.TreeSet;
+
+public class UniqeNums {
+    public static void printUniqeNums(Integer... nums) {
+        System.out.println(new TreeSet<Integer>(Arrays.asList(nums)));
+    }
+
+    public static void main(String[] args) {
+        printUniqeNums(1,2,3,4,4,5);
+    }
+}
