5ae09285fce3e7903c49e08bee91eabfc7dace06
Student <i.domnich2013@gmail.com>
2018-02-28 20:28:09 +0200

    UIS

---
+++ lesson19/atLesson/ex1/NewRunnable.java
@@ -0,0 +1,19 @@
+package lesson19.atLesson.ex1;
+
+/**
+ * Created by student on 25.02.2018.
+ */
+public class NewRunnable implements Runnable {
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+        for (int i = 0; i < 101; i++) {
+            System.out.print(i % 10 == 0 ? i + " "+Thread.currentThread() +" \n" : "");
+            try {
+                Thread.sleep(100);
+            }catch (InterruptedException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
---
+++ lesson19/atLesson/ex2/StringBuilderThreadDemo.java
@@ -0,0 +1,13 @@
+package lesson19.atLesson.ex2;
+
+/**
+ * Created by Student on 28.02.2018.
+ */
+public class StringBuilderThreadDemo {
+    public static void main(String[] args) {
+    StringBuilderThread sbt = new StringBuilderThread(new StringBuilder("a"));
+        new Thread(sbt).start();
+        new Thread(sbt).start();
+        new Thread(sbt).start();
+    }
+}
